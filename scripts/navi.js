885f0eda2d62cd1c0b8da0c12107307306fa87f3
Wesley McCann <wmccann@localhost.hsd1.co.comcast.net>
2014-04-23 23:10:00 -0600

    Changed Navi and added a real post

--- scripts/navi.js
+++ scripts/navi.js
@@ -1,27 +1,27 @@
 window.onload = function currentPage() {
     page = window.location.pathname;
     if(page == "/index.html") {
-        document.getElementById("home").style.borderTop="2px solid #30ACFF";
         document.getElementById("home").style.color="#30ACFF";
+        document.getElementById("home").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
     }
     else if(page == "/") {
-        document.getElementById("home").style.borderTop="2px solid #30ACFF";
         document.getElementById("home").style.color="#30ACFF";
+        document.getElementById("home").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
     }
     else if(page == "/about.html") {
-        document.getElementById("about").style.borderTop="2px solid #FF0000";
-        document.getElementById("about").style.color="#FF0000";
+        document.getElementById("about").style.color="#30ACFF";
+        document.getElementById("about").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
     }
     else if(page == "/team.html") {
-        document.getElementById("team").style.borderTop="2px solid #FF6600";
-        document.getElementById("team").style.color="#FF6600";
+        document.getElementById("team").style.color="#30ACFF";
+        document.getElementById("team").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
     }
     else if(page== "/games.html") {
-        document.getElementById("games").style.borderTop="2px solid #007A00";
-        document.getElementById("games").style.color="#007A00";
+        document.getElementById("games").style.color="#30ACFF";
+        document.getElementById("games").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
     }
     else if(page == "/contact.html") {
-        document.getElementById("contact").style.borderTop="2px solid #000000";
-        document.getElementById("contact").style.color="#000000";
+        document.getElementById("contact").style.color="#30ACFF";
+        document.getElementById("contact").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
     }
 }
