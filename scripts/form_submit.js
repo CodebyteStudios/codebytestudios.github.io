104cf5f9e0fe1f1ed6e0f289becadf106350c6e5
Wesley McCann <wmccann@localhost.hsd1.co.comcast.net>
2014-04-24 16:11:57 -0600

    Added ajax to contact form

---
+++ scripts/form_submit.js
@@ -0,0 +1,12 @@
+$('#ajax-form').submit(function(){
+        $.ajax({
+          dataType: 'jsonp',
+          url: "http://getsimpleform.com/messages/ajax?form_api_token=20b64bd110a5efccb4039a0e1ca5bbd6",
+          data: $('#ajax-form').serialize() 
+        }).done(function() {
+          //callback which can be used to show a thank you message
+          //and reset the form
+          alert("Success: Thank you for contacting us!");
+        });
+        return false; //to stop the form from submitting
+      });
