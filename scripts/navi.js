a4cbf33420954f9cc141c6991055aff41020d35a
OpenShift guest <septimus98@gmail.com>
2014-06-03 13:36:56 -0400

    Major site redesign.

--- scripts/navi.js
+++ scripts/navi.js
@@ -1,27 +1,21 @@
 window.onload = function currentPage() {
     var page = window.location.pathname;
     if(page == "/index.html") {
-        document.getElementById("home").style.color="#30ACFF";
-        document.getElementById("home").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
+        document.getElementById("home").style.borderTopColor="#30ACFF";
     }
     else if(page == "/") {
-        document.getElementById("home").style.color="#30ACFF";
-        document.getElementById("home").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
+        document.getElementById("home").style.borderTopColor="#30ACFF";
     }
     else if(page == "/about.html") {
-        document.getElementById("about").style.color="#30ACFF";
-        document.getElementById("about").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
+        document.getElementById("about").style.borderTopColor="#30ACFF";
     }
     else if(page == "/team.html") {
-        document.getElementById("team").style.color="#30ACFF";
-        document.getElementById("team").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
+        document.getElementById("team").style.borderTopColor="#30ACFF";
     }
     else if(page== "/games.html") {
-        document.getElementById("games").style.color="#30ACFF";
-        document.getElementById("games").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
+        document.getElementById("games").style.borderTopColor="#30ACFF";
     }
     else if(page == "/contact.html") {
-        document.getElementById("contact").style.color="#30ACFF";
-        document.getElementById("contact").style.boxShadow="inset 0 0 15px rgba(0,0,0,0.5)";
+        document.getElementById("contact").style.borderTopColor="#30ACFF";
     }
 };
